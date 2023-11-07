031e97506c3d0c25d02681ee2f8c3d934cbe12c7
Venus <venusbastola27@gmail.com>
2023-11-07 11:25:21 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,3 @@
+//i am starting a new project where i will be building a socket server
+//and implement a basic chat application backend,probably i will also do
+//basic frontend for the application
